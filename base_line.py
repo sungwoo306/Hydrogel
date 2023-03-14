ad50452b7b55bc389c46d8f2a9704c0b52b718c7
physics00 <physics113@snu.ac.kr>
2023-03-14 23:24:26 +0900

    base_line.py added

--- base_line.py
+++ base_line.py
@@ -6,13 +6,16 @@ from numpy import ndarray
 import xarray as xr
 from numpy.polynomial.polynomial import Polynomial
 import matplotlib.pyplot as plt
-import kneed
+
+# import kneed
 from jhelabtoolkit.io.nanosurf import nanosurf
 from jhelabtoolkit.utils.plotting import configure_matplotlib_defaults
 
 configure_matplotlib_defaults()
 
-filepath = "./data/afm/20230106/highly entangled hydrogel(C=1.0)/Image00801.nid"
+filepath = (
+    "./data/afm/20230106/highly entangled hydrogel(C=1.0)/Image00801.nid"
+)
 config, data = nanosurf.read_nid(filepath)
 
 
@@ -58,12 +61,18 @@ def find_contact_point(distance: ndarray, deflection: ndarray) -> float:
 
 
 def fit_baseline_polynomial(
-    distance: ndarray, deflection: ndarray, contact_point: float = 0.0, degree: int = 1
+    distance: ndarray,
+    deflection: ndarray,
+    contact_point: float = 0.0,
+    degree: int = 1,
 ) -> Polynomial:
     pre_contact = distance < contact_point
     domain = (np.amin(distance), np.amax(distance))
     return Polynomial.fit(
-        distance[pre_contact], deflection[pre_contact], deg=degree, domain=domain
+        distance[pre_contact],
+        deflection[pre_contact],
+        deg=degree,
+        domain=domain,
     )
 
 
@@ -73,36 +82,76 @@ z_bwd, defl_bwd = get_z_and_defl(backward)
 dist_fwd = calc_tip_distance(z_fwd, defl_fwd)
 dist_bwd = calc_tip_distance(z_bwd, defl_bwd)
 # cp = find_contact_point(dist_fwd, defl_fwd)
-# %%
-fig, ax = plt.subplots(1, 1, figsize=(7, 5))
-ax.plot(dist_fwd, defl_fwd, label="forward")
-ax.plot(dist_bwd, defl_bwd, label="backward")
-ax.legend()
-# %%
-cp = 1.2e-6
-dist_fwd = dist_fwd - cp
-dist_bwd = dist_bwd - cp
-baseline_poly_fwd = fit_baseline_polynomial(dist_fwd, defl_fwd)
-defl_processed_fwd = defl_fwd - baseline_poly_fwd(dist_fwd)
-baseline_poly_bwd = fit_baseline_polynomial(dist_bwd, defl_bwd)
-defl_processed_bwd = defl_bwd - baseline_poly_bwd(dist_bwd)
+#%%
+def distance_std(N1: int):
+    N2 = N1 * 2
+    std_bwd_front = np.std(dist_bwd[:N1])
+    std_bwd_rear = np.std(dist_bwd[N1:N2])
+    return np.abs(std_bwd_front - std_bwd_rear)
 
-# %%
-fig, ax = plt.subplots(1, 1, figsize=(7, 5))
-ax.plot(dist_fwd, defl_fwd, label="forward")
-ax.plot(dist_bwd, defl_bwd, label="backward")
+
+N_f = 100
+N_b = 100
+d_array_f = []
+d_array_b = []
+for i, j in zip(range(N_f), range(N_b)):
+    d_f = distance_std(i)
+    d_b = distance_std(j)
+    d_array_f.append(d_f)
+    d_array_b.append(d_b)
+
+max_diff_f = np.argmax(d_array_f[1:])
+max_diff_b = np.argmax(d_array_b[1:])
+max_diff_f, max_diff_b
+#%%
+n_f = max_diff_f * 2
+n_b = max_diff_b * 2
+A_f = np.stack((dist_fwd[:n_f], np.ones(len(dist_fwd[:n_f]))), axis=0).T
+m_f, c_f = np.linalg.lstsq(A_f, defl_fwd[:n_f], rcond=None)[0]
+
+A_b = np.stack((dist_bwd[:n_b], np.ones(len(dist_bwd[:n_b]))), axis=0).T
+m_b, c_b = np.linalg.lstsq(A_b, defl_bwd[:n_b], rcond=None)[0]
+m_f, c_f, m_b, c_b
+#%%
+
+fig, ax = plt.subplots(1, 1, figsize=(15, 10))
+ax.plot(dist_fwd, defl_fwd, ".", label="forward", markersize=3)
+ax.plot(dist_bwd, defl_bwd, ".", label="backward", markersize=3)
+ax.plot(dist_bwd, m_f * dist_bwd + c_f, ".", label="linear_reg", markersize=3)
+ax.plot(
+    dist_fwd,
+    defl_fwd - (m_f * dist_fwd + c_f),
+    ".",
+    label="base_line",
+    markersize=3,
+)
+ax.plot(dist_bwd, m_b * dist_bwd + c_b, ".", label="linear_reg", markersize=3)
+ax.plot(
+    dist_bwd,
+    defl_bwd - (m_b * dist_bwd + c_b),
+    ".",
+    label="base_line",
+    markersize=3,
+)
 ax.legend()
-# %%
-fig, ax = plt.subplots(1, 1, figsize=(7, 5))
-ax.plot(dist_fwd, defl_processed_fwd, label="forward")
-ax.plot(dist_bwd, defl_processed_bwd, label="backward")
+#%%
+fig, ax = plt.subplots(1, 1, figsize=(15, 10))
+ax.plot(
+    dist_fwd,
+    defl_fwd - (m_f * dist_fwd + c_f),
+    ".",
+    label="base_line_fwd",
+    markersize=3,
+)
+ax.plot(
+    dist_bwd,
+    defl_bwd - (m_b * dist_bwd + c_b),
+    ".",
+    label="base_line_bwd",
+    markersize=3,
+)
 ax.legend()
-# %%
-fig, ax = plt.subplots(1, 1, figsize=(7, 5))
-ax.plot(dist_fwd, z_fwd, label="forward")
-ax.plot(dist_bwd, z_bwd, label="backward")
-ax.legend()
-# %%
+#%%
 dist_total = np.concatenate((dist_fwd, dist_bwd[::-1]), axis=-1)
 defl_total = np.concatenate((defl_fwd, defl_bwd[::-1]), axis=-1)
 is_contact = dist_total >= 0
@@ -112,44 +161,9 @@ force = defl_total[is_contact] * k
 sampling_rate = get_sampling_rate(config)
 time = np.arange(len(indentation)) / sampling_rate
 
-fig, axes = plt.subplots(2, 1, figsize=(7, 5), sharex=True)
+print(indentation.shape)
+
+fig, axes = plt.subplots(2, 1, figsize=(10, 7), sharex=True)
 axes[0].plot(time, indentation)
 axes[1].plot(time, force)
-# %%
-max_ind = np.argmax(indentation)
-t_max = time[max_ind]
-t_max
-indent_max = indentation[max_ind]
-# %%
-Polynomial.fit(
-    time[: max_ind + 1],
-    indentation[: max_ind + 1],
-    deg=[
-        1,
-    ],
-).convert()
-# %%
-Polynomial.fit(
-    time[max_ind + 1 :],
-    indentation[max_ind + 1 :],
-    deg=[
-        1,
-    ],
-).convert()
-# %%
-indent_max / t_max
-# %%
-indent_max / (time[-1] - t_max)
-# %%
-v_avg = 2 * indent_max / time[-1]
-# %%
-np.savez(
-    "./Image00801.npz",
-    indentation=indentation,
-    time=time,
-    force=force,
-    v=v_avg,
-    t_max=t_max,
-)
-
-# %%
+axes[1].set_xlabel("$Time$ $t$")
